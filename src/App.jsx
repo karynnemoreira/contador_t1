d3ae3d0eaa8fabec95863220df4955c523f801ca
karynnemoreira <karynnemoreira@yahoo.com.br>
2024-02-05 10:36:46 -0300

    subindo o contador da turma t1, adicionamos 2 botões + - c

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react" //importando o react
+import "./index.css"  //Estilizando com o index.css
+import "./App.css" //Estilizando com o App.css
+import Contador from "./Contador/Contador.jsx" //importando o componente Contador.jsx
+
+function App(){
+  return(
+    <>
+    <Contador/> 
+    </>
+  )
+}
+
+export default App
